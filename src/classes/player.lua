33f6f5e7184d8ee9fa806ad40e8daf6f9162d55e
falcie <69336491+falcie1337@users.noreply.github.com>
2024-09-12 00:11:47 -0300

    Removed a game.lua file inside the classes folder because of redundancy

--- src/classes/player.lua
+++ src/classes/player.lua
@@ -6,7 +6,18 @@ function Player.init(self, x, y, world)
 	self.world = world
 	self.width = 16
 	self.height = 16
+
 	self.dx = 0
 	self.dy = 0
-	self.speed = 100
+	self.gravity = 50
+	self.jumpForce = -30
+	self.maxFallSpeed = 40
+	self.dashTimer = 0
+	self.maxDashTime = 10
+
+	self.onGround = false
+	self.canJump = false
+	self.dashing = false
+
+	self.world:add(self, self.x, self.y, self.width, self.height)
 end
